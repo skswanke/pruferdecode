7ca291a79bd4ddf46031a2d57d1b27d0b9957598
skswanke <skswanke@users.noreply.github.com>
2016-10-25 12:06:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,6 @@ function prufer() {
         code.push(temp)
     }
     if (input === "" || input === null || input === 'undefined' || isNotNum || isZero || tooBig){
-        console.log("invalid input")
         var error = document.getElementById("error")
         error.classList.add("show")
         if (input === "" || input === null || input === 'undefined'){
@@ -85,8 +84,6 @@ function prufer() {
         }
         return 0
     }
-    console.log("code:")
-    console.log(code.toString())
 
     // Remove existing nodes/links
     var nodes = []
@@ -102,15 +99,11 @@ function prufer() {
         verts.push(i+1)
     }
     var vertsLeft = verts
-    console.log("vertsLeft:")
-    console.log(vertsLeft.toString())
 
     while(vertsLeft.length>0){
         while(code.length>0){
             // Get vertices not in prufer code but are in the set of vertices
             possibleMins = vertsLeft.diff(code)
-            console.log("possibleMins:")
-            console.log(possibleMins.toString())
 
             // Get the minimum of these vertices
             minVert = Math.min.apply(Math, possibleMins)
@@ -126,13 +119,10 @@ function prufer() {
                     break
                 }
             }
-            console.log("Is " + code[0].toString() + " in nodes: " + isCodeInNodes.toString())
             if(!isCodeInNodes){
                 sourceNode = {id: code[0].toString()}
                 nodes.push(sourceNode)
                 codeNodeLoc = nodes.length - 1 
-                console.log("Code node adds:")
-                console.log(sourceNode.id)
             }
             var isMinInNodes = false
             var minNodeLoc = -1
@@ -143,41 +133,24 @@ function prufer() {
                     break
                 }
             }
-            console.log("Is " + minVert.toString() + " in nodes: " + isMinInNodes.toString())
             if(!isMinInNodes){
                 targetNode = {id: minVert.toString()}
                 nodes.push(targetNode)
                 minNodeLoc = nodes.length - 1
-                console.log("Min node adds:")
-                console.log(targetNode.id)
             }
 
             // Create an Edge between the vertices
             links.push({source: nodes[codeNodeLoc], target: nodes[minNodeLoc]})
-            console.log("Links Add:")
-            console.log("source:") 
-            console.log(sourceNode)
-            console.log("target:")
-            console.log(targetNode)
 
             // Remove vertex from prufer code
             code.shift()
-            console.log("code:")
-            console.log(code.toString())
 
             // Remove min vertex from remain vertices
             vertsLeft.splice(vertsLeft.indexOf(minVert), 1)
-            console.log("vertsLeft:")
-            console.log(vertsLeft.toString())
-            console.log("Nodes:")
-            for(var j=0; j<nodes.length;j++){
-                console.log(nodes[j])
-            }
-            console.log("--------------------------------------------------------")
+            
             // Restart force layout
             restart()
         }
-        console.log("Verts Left: " + vertsLeft.toString())
 
         //Connect final remaining vertices in remaining vertices
         var is0InNodes = false
@@ -198,32 +171,16 @@ function prufer() {
             targetNode = {id: vertsLeft[1].toString()}
             nodes.push(targetNode)
             links.push({source: nodes[zeroLoc], target: nodes[nodes.length-1]})
-            console.log("Final Add:")
-            console.log(nodes[nodes.length-1].id)
-            console.log("Final Links Add:")
-            console.log("source:") 
-            console.log(nodes[zeroLoc])
-            console.log("target:")
-            console.log(nodes[nodes.length-1])
         } else if (is1InNodes && !is0InNodes) {
             targetNode = {id: vertsLeft[0].toString()}
             nodes.push(targetNode)
             links.push({source: nodes[oneLoc], target: nodes[nodes.length-1]})
-            console.log("Final Add:")
-            console.log(targetNode.id)
-            console.log("Final Links Add:")
-            console.log("source:") 
-            console.log(nodes[zeroLoc])
-            console.log("target:")
-            console.log(nodes[nodes.length-1])
         } else if (is0InNodes && is1InNodes) {
             links.push({source: nodes[oneLoc], target: nodes[zeroLoc]})
         }
         vertsLeft = []
 
         restart()
-        console.log("Nodes:")
-        console.log(nodes)
     }
 }
 
